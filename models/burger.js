423f0d12a38b6f01c0f9301a856ca90467f84535
ebeaudet <ebeaudet@lsac.org>
2019-02-22 13:45:27 -0500

    orm and burger.js

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    create: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+
+    update: function(objectColVals, condition, cb) {
+        orm.update("burgers", objectColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
+
