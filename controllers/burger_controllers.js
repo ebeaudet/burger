2d7d1fc6c3a83a7fe287f2af720c488281470390
unknown <ed.beaudet@gmail.com>
2019-02-22 21:52:32 -0500

    almost there

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
+    burger.selectAll(function(data) {
         var hbsObject = {
             burger: data
         };
@@ -13,7 +13,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("api/burgers", function(req, res) {
-    burger.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+    burger.insertOne(["burger_name"], [req.body.name], function(result) {
         res.json({id: result.insertId});
     });
 });
@@ -23,7 +23,7 @@ router.put("api/burgers/:id", function(req,res) {
 
     console.log("condition", condition);
 
-    burger.update(
+    burger.updateOne(
         {
             devoured: req.body.devoured
         },
--- models/burger.js
+++ models/burger.js
@@ -1,20 +1,20 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function(cb) {
-        orm.all("burgers", function(res) {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
 
-    create: function(cols, vals, cb) {
-        orm.create("burgers", cols, vals, function(res) {
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
 
-    update: function(objectColVals, condition, cb) {
-        orm.update("burgers", objectColVals, condition, function(res) {
+    updateOne: function(objectColVals, condition, cb) {
+        orm.updateOne("burgers", objectColVals, condition, function(res) {
             cb(res);
         });
     }
