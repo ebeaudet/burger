bd2f3c4ab4cbce8cc7916962b9b2144d134ce062
unknown <ed.beaudet@gmail.com>
2019-02-27 18:04:33 -0500

    some styling

--- config/connection.js
+++ config/connection.js
@@ -21,34 +21,7 @@ connection.connect(function(err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-  console.log("are we here")
   console.log("connected as id " + connection.threadId);
 });
-console.log("end of connectiosn")
 // Export connection for our ORM to use.
 module.exports = connection;
-
-
-
-// // Set up MySQL connection.
-// var mysql = require("mysql");
-
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "MySQLP@ssw0rd",
-//   database: "burgers_db"
-// });
-
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// // Export connection for our ORM to use.
-// module.exports = connection;
